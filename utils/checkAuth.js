5f1a0a78bd615ac00f898af2f45fac3a84fe0efc
Alexander <7jobbox7@mail.ru>
2022-12-29 15:38:52 +0300

    3 commit
    delete post

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken';
 
-export default (req,res,next) => {
-    const token = (req.headers.authorization || '').replace(/Bearer\s?/, ''); // если пришёл токен или ничего вырежи слово bearer и вместо него вставь пустую строку
-    
-    if (token) {
-        try{
-            const decoded = jwt.verify(token, 'secret123')     //расшифровываем токен
+export default (req, res, next) => {
+  const token = (req.headers.authorization || '').replace(/Bearer\s?/, ''); // если пришёл токен или ничего вырежи слово bearer и вместо него вставь пустую строку
 
-            req.userId = decoded._id;
-            next();
-        }catch (e) {
-            return res.status(403).json({
-                message:'нет доступа'})
-        }
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, 'secret123'); //расшифровываем токен
 
-    } else {
-        return res.status(403).json({
-            message:'нет доступа'})
+      req.userId = decoded._id;
+      next();
+    } catch (e) {
+      return res.status(403).json({
+        message: 'нет доступа',
+      });
     }
-  
-}
+  } else {
+    return res.status(403).json({
+      message: 'нет доступа',
+    });
+  }
+};
